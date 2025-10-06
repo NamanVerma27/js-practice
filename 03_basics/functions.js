029ae772894e7377d9bd2ac2d843d6fb527980d9
Naman Verma <141406285+NamanVerma27@users.noreply.github.com>
2025-10-06 16:29:35 +0000

    function done

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -14,3 +14,10 @@ addTwoNumbers;
 function addTwoNumbers2(num1, num2 = 5) { // if num2 is not passed, it will take default value 5
     return num1 + num2;
 }
+
+//// rest parameter - it takes multiple arguments and puts them in an array
+function calculateCartPrice(...num1) { 
+    return num1
+}
+// if parameter : (val1 , val2 , ...num1) - it will take first two values in val1 and val2 and rest in num1 array
+calculateCartPrice(200 , 400 , 500)
